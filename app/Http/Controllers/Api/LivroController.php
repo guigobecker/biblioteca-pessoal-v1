a172f4fa7e113a4f1ac694409053bba64ad06c50
Rodrigo Becker <rsbecker@inf.ufsm.br>
2023-10-14 17:19:49 -0300

    get /livros implementado corretamente

--- app/Http/Controllers/Api/LivroController.php
+++ app/Http/Controllers/Api/LivroController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Livro;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class LivroController extends Controller
 {
     public function index() {
-        return 'estou aqui!';
+        $livros = Livro::all();
+
+        if ($livros->count() > 0) {
+            $dados = [
+                'status' => 200,
+                'livros' => $livros
+            ];
+    
+            return response()->json($dados, 200);
+        }
+
+        else {
+            $dados = [
+                'status' => 404,
+                'mensagem' => 'Nenhum livro encontrado.'
+            ];
+    
+            return response()->json($dados, 404);
+        }
+
     }
 }
